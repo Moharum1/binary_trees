05df3aeb4c2e5998d01f81862ea1d3e45322fcfc
Moharum1 <moharum4@gamil.com>
2024-02-28 16:46:42 +0200

    Done

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,27 +13,22 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *right_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *right_node = binary_tree_node(parent, value);
 
 	if (right_node == NULL)
 		return (NULL);
-
-	right_node->parent = parent;
-	right_node->n = value;
-	right_node->right = NULL;
 
 	parent_right_node = parent->right;
 	if (parent_right_node != NULL)
 	{
 		right_node->right = parent_right_node;
 		parent_right_node->parent = right_node;
-		parent->right = right_node;
 	}
 	else
 	{
-		parent->right = right_node;
 		right_node->right = NULL;
 	}
+	parent->right = right_node;
 
 	return (right_node);
 }
