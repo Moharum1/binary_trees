978b79329b8a93a10754217bc074f170819200a7
Moharum1 <moharum4@gamil.com>
2024-02-28 16:49:15 +0200

    Done

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,11 +9,12 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *parent_right_node;
+	binary_tree_t *right_node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *right_node = binary_tree_node(parent, value);
+	right_node = binary_tree_node(parent, value);
 
 	if (right_node == NULL)
 		return (NULL);
